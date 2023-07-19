66d1ba9f501c696b3bbe151455079f1e8e077edb
wafa <132163343+wafaMAL@users.noreply.github.com>
2023-07-19 09:38:30 +0300

    Update Error.js

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,22 +2,80 @@ import React from "react";
 import styled from "styled-components";
 import Button from "../Button";
 import { useHistory } from "react-router-dom";
-import { ModalHeader } from "../MasterCss";
+import HowToPlayModal from "../LandingScreen/HowToPlayModal";
+import FunFactsModal from "../LandingScreen/FunFacts";
+import { spinscale } from "../Keyframes";
+
+import { ReactComponent as EarthSvg } from "../../assets/earth.svg";
 import { ReactComponent as StarsSvg } from "../../assets/stars.svg";
+
+import UIfx from "uifx";
 
 const Container = styled.div`
   background-image: linear-gradient(#060606, #08345c);
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-around;
   height: 100vh;
   width: 100vw;
   z-index: -10;
 `;
 
+const Title = styled.h1`
+background-color: #ff8a00;
+  font-family: 'IBM Plex Sans Arabic', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 72px;
+  text-align: center;
+  line-height: 130px;
+  text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+  color: transparent;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  margin-bottom: 0;
+  position: relative;
+  top: 10%;
+  z-index: 5;
+`;
 
-const Stars = styled(StarsSvg)`
+const TitleBig = styled.h1`
+background-color: #ff8a00;
+font-family: 'IBM Plex Sans Arabic', sans-serif;
+text-align: center;
+font-size: 110px;
+font-weight: bold;
+// -webkit-text-stroke: 1px black;
+text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+color: transparent;
+-webkit-background-clip: text;
+-moz-background-clip: text;
+background-clip: text;
+margin-block-start: 0em;
+margin-block-end: 0em;
+z-index: 5;
+`;
+
+
+const Earth = styled.div`
+  position: absolute;
+  bottom: 0px;
+  left: -150px;
+  overflow: hidden;
+`;
+
+const ButtonContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  justify-content: space-around;
+  margin-bottom: 0.5em;
+`;
+
+const Stars = styled.div`
   position: absolute;
   top: 0;
   left: 0;
@@ -29,24 +87,64 @@ const Stars = styled(StarsSvg)`
   align-items: flex-start;
   justify-content: space-around;
   overflow: hidden;
-  z-index: 0;
+  animation: ${ spinscale } 4s linear infinite alternate;
 `;
 
-const Error = props => {
+const LandingScreen = props => {
   const history = useHistory();
 
-  const goHome = () => {
-    history.push("/");
+ 
+
+  const startGame = () => {
+   history.push("/game")
   };
 
   return (
+    
     <Container>
-      <ModalHeader>error: 404</ModalHeader>
-      <ModalHeader>الرجاء الضغط على الزر</ModalHeader>
-      <Stars />
-      <Button primary handleClick={goHome} label={"اضغط هنا"} />
+      <Title> قلّل كرّر دوّر</Title>
+      <br></br>
+      <TitleBig></TitleBig>
+
+    
+      <Stars>
+        <StarsSvg />
+      </Stars>
+      <Earth>
+        <EarthSvg />
+      </Earth>
+      <Button
+        primary
+        type="button"
+        renderAs="button"
+        handleClick={startGame}
+        label="أبدا"
+      ></Button>
+     
+      <ButtonContainer>
+        {props.howToPlayModal && (
+          <HowToPlayModal
+            handleClose={props.hideHowToPlayModal}
+          ></HowToPlayModal>
+        )}
+        {props.funFactsModal && (
+          <FunFactsModal handleClose={props.hideFunFactsModal}></FunFactsModal>
+        )}
+        <Button
+          type="button"
+          handleClick={props.showHowToPlayModal}
+          label="كيفية اللعب"
+        ></Button>
+        <Button
+          type="button"
+          handleClick={props.showFunFactsModal}
+          label="معلومات عن إعادة التدوير  "
+        ></Button>
+      </ButtonContainer>
     </Container>
+   
+    
   );
 };
 
-export default Error;
+export default LandingScreen;
